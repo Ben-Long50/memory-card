88f61dd7066314ab3ab51aa32e8feb165a3a56d5
Ben-Long50 <benjlong50@gmail.com>
2024-06-06 10:16:12 -0700

    adjusting styling for mobile responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { mdiGithub } from '@mdi/js';
 export default function Footer() {
   return (
     <footer className="footer font">
-      <Icon path={mdiGithub} size={1.5} />
+      <Icon path={mdiGithub} size={'clamp(1.5rem, 3vw, 2.5rem'} />
       <a href="https://github.com/Ben-Long50">Ben-Long50</a>
     </footer>
   );
